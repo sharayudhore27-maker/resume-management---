a8a7b96b572fa2ab27441aad1a4ff5ee2d175ea3
sharayudhore27-maker <sharayudhore27@gmail.com>
2025-12-19 10:12:17 +0530

    Add files via upload

---
+++ firebase (1).js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDHFRcA0wOPjWRC4Er6WepE9y7Xo-7LM_8",
+  authDomain: "resume-management-system-a08e8.firebaseapp.com",
+  projectId: "resume-management-system-a08e8",
+  storageBucket: "resume-management-system-a08e8.firebasestorage.app",
+  messagingSenderId: "598577009220",
+  appId: "1:598577009220:web:456e143e398f6c396e6211",
+  measurementId: "G-2HT2PVYSZE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db };
+
